e91982d7794d46e4544d3bcf9817532864765e38
drios9107 <david.rios@gmail.com>
2021-11-18 09:53:20 -0500

    created component for messages

---
+++ resources/js/components/MessageComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="alert alert-success" v-if="message">
+    {{ this.message }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["message"],
+  data() {
+    return {};
+  },
+};
+</script>
